a04a5a3398571121e880fe8784ba9216c51f2e7c
vasvladal <vasvladal@users.noreply.github.com>
2026-09-09 20:50:05 +0300

    2.Изменил  Файл build.gradle.kts (в корне проекта, не в модуле!)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    alias(libs.plugins.gradle.versions)
+    alias(libs.plugins.version.catalog.update)
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.compose) apply false
+    alias(libs.plugins.jetbrains.kotlin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
 }
