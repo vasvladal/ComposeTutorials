eff9cd83f2771be61a855acda24b6b4c23c5a6ea
vasvladal <vasvladal@users.noreply.github.com>
2026-09-09 21:41:42 +0300

    Изменил код в MainActivity.kt

--- Tutorial1-1Basics/src/main/java/com/example/composetutorials/MainActivity.kt
+++ Tutorial1-1Basics/src/main/java/com/example/composetutorials/MainActivity.kt
@@ -1,47 +1,26 @@
-package com.example.composetutorials
+package com.smarttoolfactory.tutorial1_1basics
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composetutorials.ui.theme.ComposeTutorialsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ComposeTutorialsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Вызываем наш учебный экран с BottomDrawer
+                    Tutorial2_10Screen3()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeTutorialsTheme {
-        Greeting("Android")
     }
 }
